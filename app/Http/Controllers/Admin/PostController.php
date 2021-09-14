73323e4a95b57b85fe481603382cb889ba2d9a91
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-09-14 15:42:13 +0200

    Edit Done

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -73,9 +73,9 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Post $post)
     {
-        //
+        return view('admin.posts.edit', compact('post'));
     }
 
     /**
@@ -85,9 +85,15 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request,Post $post)
     {
-       
+       $data = $request->all();
+
+       $data['slug'] = Str::slug($data['title'], '-');
+
+       $post->update($data);
+
+       return redirect()->route('admin.posts.index')->with('updated', 'Update Element'); 
     }
 
     /**
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,6 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container"></div>
+<div class="container">
+    <form action="{{ route('admin.posts.update', $post->id)}}" method="post">
+        @csrf
+        @method('PATCH')
+
+    <div class="mb-3">
+        <label for="title" >Write Title</label>
+        {{-- Remeber the value  --}}
+        <input type="title" name="title" class="form-control" id="title" value="{{$post->title}}" >
+   
+      </div>
+      <div class="mb-3">
+        <label for="descrizione" class="form-label">Descrizione</label>
+        {{-- Not value because is a TextArea --}}
+        <textarea class="form-control" type="text" name="content" cols="30" rows="10"  id="descrizione" >{{$post->content}}</textarea>
+        
+      </div>
+      <button type="submit" class="btn btn-success">Submit</button>
+    </form>
+
+</div>
 
 @endsection
